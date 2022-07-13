25ae939ae98d7d09ee66ac23b12540a0e9fcb757
NightCat <642163903@qq.com>
2022-07-13 23:05:03 +0800

    chore: add submodule

--- vite.template.config.ts
+++ vite.template.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
 const libDir = path.resolve(__dirname, 'src/components/libs')
 
 export default defineConfig({
   plugins: [vue({
     reactivityTransform: true
-  }), viteSingleFile()],
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
