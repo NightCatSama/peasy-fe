55573bc05c38cdb24e7360726909629ac0217c40
NightCat <642163903@qq.com>
2022-07-19 14:53:45 +0800

    chore: build

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import * as path from 'path'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'no-code',
   plugins: [vue({
     reactivityTransform: true
   }), svgLoader({
