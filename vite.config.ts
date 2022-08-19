4cf91523ed3e4ef72c0dcf2cd50c60d1a0e9b823
NightCat <642163903@qq.com>
2022-08-19 19:10:21 +0800

    chore: build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,20 @@ import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 import { visualizer } from 'rollup-plugin-visualizer'
 
+const packageJSON = require('./package.json')
+
+const htmlPlugin = () => {
+  return {
+    name: 'html-transform',
+    transformIndexHtml(html: string) {
+      return html.replace(
+        /\/\* app version \*\//,
+        packageJSON.version
+      )
+    }
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: loadEnv(mode, process.cwd(), '').VITE_URL_BASE,
@@ -14,6 +28,7 @@ export default defineConfig(({ mode }) => ({
     svgLoader({
       defaultImport: 'component'
     }),
+    htmlPlugin(),
     loadEnv(mode, process.cwd(), '').VITE_ANALYZE
       ? visualizer({ emitFile: true })
       : null
