7c1547735f51bd0326d069dbbbc80b1caebd10fe
NightCat <642163903@qq.com>
2022-08-19 19:12:00 +0800

    chore: add deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script is used to deploy the application to the server.
+# It is called by the deploy stage in the .travis.yml file.
+
+npm run build
+git add .
+git commit -m "chore: build"
+git push origin main
+
+exit 1
