5932cb534386dff5bb0a9ccad2a6ac8135b1b6bf
NightCat <642163903@qq.com>
2022-08-25 14:18:00 +0800

    chore: build

--- vite.config.ts
+++ vite.config.ts
@@ -40,8 +40,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
-    sourcemap: true,
-    minify: false,
+    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: {
