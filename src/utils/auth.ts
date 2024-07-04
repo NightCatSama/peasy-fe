b2e081a7d1dae548723b293380c95d4c4954cd43
NightCat <642163903@qq.com>
2024-07-04 16:40:49 +0800

    fix: signout

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,6 +2,6 @@ export const signInGithub = () =>
   window.open(`https://github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_GITHUB_CLIENT_ID}`)
 
 export const signout = () => {
-  localStorage.removeItem('accessToken')
+  localStorage.removeItem('access_token')
   location.reload()
 }
